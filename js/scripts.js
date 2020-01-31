a3963a814be4b8482735465cac35967d6ab41807
Jack Dunning <JackStunning9001@gmail.com>
2020-01-31 11:04:53 -0800

    Add html and js back end for it

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
-function Pizza(toppings, size,){
+function Pizza(toppings, size, number){
   this.toppings = toppings;
   this.size = size;
+  this.number = number
 };
 
-// when button is clicked take size and num inputs
-
-/*
-  function findTopping(arr){
-    arr.forEach(topping => {
-
-      if ()
-
-
-    })
-  }
-
-
-
-
-*/
+var pizzas = []
 
 $(document).ready(function(){
+  
+  $("#orderButton").click(function(event){
+    event.preventDefault();
 
+    var toppings = $("#toppings").val();
+    var sizes = $("#sizes").val();
+    var number = $("#number").val();
 
+    var pizza = new Pizza(toppings, sizes, number)
+    pizzas.push(pizza);
+    console.log(pizzas)
 
+    for(var i = 0; i < pizzas.length; i++) {
+      $("#orderBox").append(`${pizzas[i].toppings} <br> ${pizzas[i].size} <br> ${pizzas[i].number}`)
 
-
-
-  $("#orderButton").click(function(){
+    }
 
   })
 
