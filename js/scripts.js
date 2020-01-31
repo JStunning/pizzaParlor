25a2c11384f69ad45c31e9322f9ddd5dc3bc1bde
Jack Dunning <JackStunning9001@gmail.com>
2020-01-31 11:23:53 -0800

    Add price and receipt functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ function Pizza(toppings, size, number){
   this.toppings = toppings;
   this.size = size;
   this.number = number
+
+  if (size == "Small"){
+    this.price = 5 * this.number
+  } else if (size == "Medium") {
+    this.price = 10 * this.number
+  } else {
+    this.price = 15 * this.number
+  }
+
+  console.log(this.price)
 };
 
 var pizzas = []
@@ -19,11 +29,10 @@ $(document).ready(function(){
     pizzas.push(pizza);
     console.log(pizzas)
 
-    for(var i = 0; i < pizzas.length; i++) {
-      $("#orderBox").append(`${pizzas[i].toppings} <br> ${pizzas[i].size} <br> ${pizzas[i].number}`)
-
+    for(var i = 0; i < pizzas.length; i++){
+      $("#orderBox").text("Your Receipt")
+      $("#orderBox").append(`<br> ${pizzas[i].number}x ${pizzas[i].toppings} <br> ${pizzas[i].size} <br> $${pizzas[i].price}`);
     }
-
   })
 
 });
