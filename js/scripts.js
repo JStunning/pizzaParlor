90140a235316933f0580a48d6650ac599fd813a3
Jack Dunning <JackStunning9001@gmail.com>
2020-01-31 10:48:49 -0800

    Change I redid alot of the html to be easy for my back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
-function Pizza(toppings, size){
-  this.toppings = toppings
-  this.size = size
-}
+function Pizza(toppings, size,){
+  this.toppings = toppings;
+  this.size = size;
+};
+
+// when button is clicked take size and num inputs
+
+/*
+  function findTopping(arr){
+    arr.forEach(topping => {
+
+      if ()
+
+
+    })
+  }
+
+
+
+
+*/
+
+$(document).ready(function(){
+
+
+
+
+
+
+  $("#orderButton").click(function(){
+
+  })
+
+});
 
